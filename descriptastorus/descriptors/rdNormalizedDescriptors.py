182d2a6169aeb11b8d95cbf8ddca22baac759036
Jumana Dakka <jumana.dakka@schrodinger.com>
2021-04-30 16:40:05 -0400

    pass on warning for normalization

--- descriptastorus/descriptors/rdNormalizedDescriptors.py
+++ descriptastorus/descriptors/rdNormalizedDescriptors.py
@@ -53,7 +53,7 @@ for name, (dist, params, minV,maxV,avg,std) in dists.dists.items():
 
 for name in rdDescriptors.FUNCS:
     if name not in cdfs:
-        logging.warning("No normalization for %s", name)
+	continue
 
 def applyNormalizedFunc(name, m):
     if name not in cdfs:
